84df734d3203a638f7841b785d617bc36026233f
Pawar, Deepak Atmaram <deepawar@deloitte.com>
2023-03-01 20:23:23 +0530

    Added changes for contact form

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from '../components/Navbar'; 
 import Footer from '../components/Footer';
 import HeroImg2 from '../components/HeroImg2';
+import Form from '../components/Form';
 function Contact(){
 
 
     return <div>
         <Navbar/>
         <HeroImg2 heading="CONTACT" text="Feel free to say HI !!"/>
+        <Form/>
         <Footer/>
     </div>
 }
